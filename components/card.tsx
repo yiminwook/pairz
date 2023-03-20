29d4b9c305589167da30aaad39ac57d48d54e2ed
yiminwook <grs0416@gmail.com>
2023-03-20 22:55:12 +0900

    Add: Card component

--- components/card.tsx
+++ components/card.tsx
@@ -1,11 +1,53 @@
-import Image from "next/image";
+/* eslint-disable @next/next/no-img-element */
 import card from "@/styles/card.module.scss";
+import { FC, useRef, useState } from "react";
 
-const Card = () => {
+interface Props {
+  imgURL: string;
+  isPreView?: boolean;
+}
+
+/** isPreView가 true이면
+ *
+ *  preView 모드
+ */
+const Card: FC<Props> = ({ imgURL, isPreView }) => {
+  const [failToGetImage, setFailToGetImage] = useState<boolean>(false);
+  const cardRef = useRef<HTMLDivElement>(null);
+
+  const handleEffect = () => {
+    if (cardRef.current) {
+      const { classList, style } = cardRef.current;
+      if (isPreView !== true) {
+        classList.toggle(card.flip);
+      }
+      if (isPreView === true) {
+        if (!style.animationPlayState) {
+          style.animationPlayState = "paused";
+        } else {
+          style.animationPlayState = "";
+        }
+      }
+    }
+  };
+
   return (
     <>
-      <div>
-        <div></div>
+      <div
+        onClick={handleEffect}
+        className={[card.container, isPreView ? card.preview : ""].join(" ")}
+        ref={cardRef}
+      >
+        <div className={card.front}>
+          <div className={card.front_image_container}>
+            <img width={200} height={300} alt="card_img" src={imgURL} />
+          </div>
+        </div>
+        <div className={card.back}>
+          <div className={card.back_image_container}>
+            <img src="/loading_icon.png" alt="card_back_img" />
+          </div>
+        </div>
       </div>
     </>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import home from "@/styles/Home.module.scss";
 import ServiceLayout from "../components/service_layout";
 import { NextPage } from "next";
+import Card from "@/components/card";
 
 const IndexPage: NextPage = () => {
   return (
@@ -15,6 +16,10 @@ const IndexPage: NextPage = () => {
             <button className={home.orange}>button</button>
             <button className={home.red}>button</button>
           </div>
+          <Card
+            isPreView={true}
+            imgURL="https://pairs-img-bucket-2.s3.ap-northeast-2.amazonaws.com/%EB%AA%A8%EB%B0%94%EC%9D%BC%ED%85%8C%EC%8A%A4%ED%8A%B8"
+          />
         </div>
       </main>
     </ServiceLayout>
