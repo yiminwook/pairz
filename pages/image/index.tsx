b027909cb7399944d927a67a3421a7ce9bbbc263
yiminwook <grs0416@gmail.com>
2023-03-20 18:23:59 +0900

    Update: image page client side data fetching

--- pages/image/index.tsx
+++ pages/image/index.tsx
@@ -1,19 +1,14 @@
+/* eslint-disable @next/next/no-img-element */
+import { NextPage } from "next";
+import { FormEvent } from "react";
 import ServiceLayout from "@/components/service_layout";
 import { ImageResultWithIdx } from "@/models/image/image.model";
 import { ImageInfo } from "@/models/Info";
-import { getBaseURL } from "@/utils/get_base_url";
 import axios, { AxiosResponse } from "axios";
-import { GetStaticProps, NextPage } from "next";
-import Image from "next/image";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import imageSearch from "@/styles/img_search.module.scss";
-import { FormEvent } from "react";
 
-interface Props {
-  staticImageData: ImageInfo[];
-  staticTotal: number;
-  staticLastIdx: number | null;
-}
+interface Props {}
 
 interface ImageSearchElements extends HTMLFormControlsCollection {
   image_search__select: HTMLSelectElement;
@@ -26,15 +21,11 @@ interface ImageSearch extends HTMLFormElement {
 }
 
 /** default 이미지 이름으로 조회*/
-const ImageSearchPage: NextPage<Props> = ({
-  staticImageData,
-  staticTotal,
-  staticLastIdx,
-}) => {
-  const [failToGetImage, setFailToGetImage] = useState(false);
-  const [reqImageData, setReqImageData] = useState([...staticImageData]);
-  const [lastIdx, setLastIdx] = useState(staticLastIdx);
-  const [total, setTotal] = useState(staticTotal);
+const ImageSearchPage: NextPage<Props> = () => {
+  const [failToGetImage, setFailToGetImage] = useState<boolean>(false);
+  const [reqImageData, setReqImageData] = useState<ImageInfo[]>([]);
+  const [lastIdx, setLastIdx] = useState<number>(0);
+  const [total, setTotal] = useState(0);
   const formRef = useRef<ImageSearch>(null);
   const inputRef = useRef<HTMLInputElement>(null);
   const selectRef = useRef<HTMLSelectElement>(null);
@@ -43,31 +34,58 @@ const ImageSearchPage: NextPage<Props> = ({
     e.preventDefault();
   };
 
+  const getData = async () => {
+    try {
+      const result: AxiosResponse<ImageResultWithIdx> = await axios.get(
+        "/api/image.get"
+      );
+      const { imageData, total, lastIdx } = result.data;
+      if (
+        !(
+          result.status === 200 &&
+          total >= 0 &&
+          lastIdx !== null &&
+          lastIdx >= 0
+        )
+      )
+        throw new Error("axios err");
+      setReqImageData((pre) => [...imageData]);
+      setTotal((_pre) => total);
+      setLastIdx((_pre) => lastIdx);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   const handleGetData = async () => {
     try {
       const result: AxiosResponse<ImageResultWithIdx> = await axios.get(
         `/api/image.get?idx=${lastIdx ? lastIdx - 1 : ""}`
       );
-      const data = result.data;
-      setReqImageData((pre) => [...pre, ...data.imageData]);
-      setTotal((_pre) => data.total);
-      setLastIdx((_pre) => data.lastIdx);
+      if (
+        !(
+          result.status === 200 &&
+          result.data.total >= 0 &&
+          result.data.lastIdx !== null &&
+          result.data.lastIdx >= 0
+        )
+      )
+        throw new Error("axios err");
+      setReqImageData((pre) => [...pre, ...result.data.imageData]);
+      setTotal((_pre) => result.data.total);
+      setLastIdx((_pre) => result.data.lastIdx as number);
     } catch (err) {
       console.error(err);
     }
   };
 
   const handleImageSearchReset = () => {
-    if (selectRef.current) {
-      selectRef.current.value = "imageName";
-    }
-    if (inputRef.current) {
-      inputRef.current.value = "";
-    }
-    setReqImageData((pre) => staticImageData);
-    setTotal((_pre) => staticTotal);
-    setLastIdx((_pre) => staticLastIdx);
+    getData();
   };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <ServiceLayout title="test">
@@ -91,13 +109,13 @@ const ImageSearchPage: NextPage<Props> = ({
               reqImageData.map((data) => (
                 <div key={data.id}>
                   <div className={imageSearch.contents_image_container}>
-                    <Image
+                    <img
                       src={data.imgURL}
                       width={200}
                       height={300}
                       alt="card_img"
                       onError={() => setFailToGetImage((_pre) => true)}
-                    ></Image>
+                    />
                   </div>
                   <div className={imageSearch.contents_name}>
                     {data.imageName}
@@ -111,36 +129,6 @@ const ImageSearchPage: NextPage<Props> = ({
       </main>
     </ServiceLayout>
   );
-};
-
-export const getStaticProps: GetStaticProps<Props> = async () => {
-  try {
-    const baseURL = getBaseURL(true);
-    const result: AxiosResponse<ImageResultWithIdx> = await axios.get(
-      baseURL + "/api/image.get"
-    );
-    if (result.status !== 200) {
-      return {
-        props: { staticImageData: [], staticTotal: 0, staticLastIdx: null },
-        revalidate: 30,
-      };
-    }
-    const { imageData, total, lastIdx } = result.data;
-    return {
-      props: {
-        staticImageData: imageData,
-        staticTotal: total,
-        staticLastIdx: lastIdx,
-      },
-      revalidate: 30,
-    };
-  } catch (err) {
-    console.error(err);
-    return {
-      props: { staticImageData: [], staticTotal: 0, staticLastIdx: null },
-      revalidate: 30,
-    };
-  }
 };
 
 export default ImageSearchPage;
